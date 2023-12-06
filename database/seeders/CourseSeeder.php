471f09c4e3713f61220acb57d5dfc312556f3734
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-07 02:05:27 +0800

    Update CourseSeeder.php

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -12,6 +12,22 @@ class CourseSeeder extends Seeder
      */
     public function run(): void
     {
-        Course::factory()->count(5)->create();
+        Course::create(['name' => 'Advertising and Marketing Management']);
+        Course::create(['name' => 'Agronomy']);
+        Course::create(['name' => 'Animation and Multimedia Design']);
+        Course::create(['name' => 'Basic Education']);
+        Course::create(['name' => 'Biofuel Production Technologies']);
+
+        Course::create(['name' => 'Communication Design']);
+        Course::create(['name' => 'Equinculture']);
+        Course::create(['name' => 'Informatics Engineering']);
+        Course::create(['name' => 'Journalism and Communication']);
+        Course::create(['name' => 'Management']);
+
+        Course::create(['name' => 'Nursing']);
+        Course::create(['name' => 'Oral Hygiene']);
+        Course::create(['name' => 'Social Service']);
+        Course::create(['name' => 'Tourism']);
+        Course::create(['name' => 'Veterinary Nursing']);
     }
 }
