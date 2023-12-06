e232611870ff6af9517fefbc5e913292780fbcf5
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-07 02:23:09 +0800

    Update OccupationSeeder.php

--- database/seeders/OccupationSeeder.php
+++ database/seeders/OccupationSeeder.php
@@ -12,6 +12,22 @@ class OccupationSeeder extends Seeder
      */
     public function run(): void
     {
-        Occupation::factory()->count(5)->create();
+        Occupation::create(['name' => 'Student']);
+        Occupation::create(['name' => 'Director and Executive Manager']);
+        Occupation::create(['name' => 'Specialist']);
+        Occupation::create(['name' => 'Technician']);
+        Occupation::create(['name' => 'Administrative Staff']);
+
+        Occupation::create(['name' => 'Security and Safety Worker']);
+        Occupation::create(['name' => 'Skilled Worker']);
+        Occupation::create(['name' => 'Machine Operator']);
+        Occupation::create(['name' => 'Unskilled Worker']);
+        Occupation::create(['name' => 'Armed Forces Profession']);
+
+        Occupation::create(['name' => 'Health Professional']);
+        Occupation::create(['name' => 'Teacher']);
+        Occupation::create(['name' => 'Seller']);
+        Occupation::create(['name' => 'Cleaning Worker']);
+        Occupation::create(['name' => 'Office Worker']);
     }
 }
